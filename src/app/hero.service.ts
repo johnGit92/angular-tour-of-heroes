236037a6462927731335834f0f5459302eb926af
johnGit92 <gnb.celato@gmail.com>
2019-05-13 10:24:51 +0200

    Added Observable and MessageService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({ // Injectable meta data
   providedIn: 'root' // inject the service at the root level, making it available everywhere it is requested
 })
 export class HeroService {
 
-  constructor() { }
-  getHeroes(): Hero[] {
-    return HEROES;
+  // MessageService injection
+  constructor(private messageService: MessageService) { }
+
+  // Returns an Observable<Hero[]> that emits a single value, the array of mock heroes
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 }
